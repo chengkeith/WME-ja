d5347a8648e568b858ae84ff6b7811fa5d26b970
Joe <crazycaveman@users.noreply.github.com>
2017-04-25 12:19:23 -0400

    Update wme_junctionangle.user.js
    Update version to push changes in Spanish translation

--- wme_junctionangle.user.js
+++ wme_junctionangle.user.js
@@ -4,7 +4,7 @@
 // @description   Show the angle between two selected (and connected) segments
 // @include       /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/.*$/
 // @updateURL     https://github.com/milkboy/WME-ja/raw/master/wme_junctionangle.user.js
-// @version       1.13.4
+// @version       1.13.4.1
 // @grant         none
 // @copyright     2016 Michael Wikberg <waze@wikberg.fi>
 // @license       CC-BY-NC-SA
